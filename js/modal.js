696f5ad27607887b4e115af64aab1d45a66e3461
Giselle Borrero <giselle.a.borrero@gmail.com>
2021-11-16 17:04:25 -0500

    modal update

--- js/modal.js
+++ js/modal.js
@@ -3,5 +3,10 @@ function modalClick() {
     var modal = document.getElementById("myModal");
     console.log(modal);
     modal.style.display = "block";
+}
+
+function closeModal() {
+
+    document.getElementById("myModal").style.display = "none";
 
 }
