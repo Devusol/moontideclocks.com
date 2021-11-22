3b47c398427f2b21295f9d7cafbb701cc5f49e7c
Giselle Borrero <giselle.a.borrero@gmail.com>
2021-11-22 13:37:40 -0500

    giselle no learn mern

---
+++ frontend/src/App.js
@@ -0,0 +1,58 @@
+import React from 'react';
+
+function Products() {
+    return (
+
+
+
+
+
+    <section id="clocksSection">
+        <div className="row">
+            <div className="col-md-4 col-lg-4 text-center  mb-4 mb-md-0">
+                <a href="./about.html">
+                    <img src="images/Charybdis.png" alt="Charybdis Clock"></img>
+                    <h5>Tidal Charybdis</h5>
+                </a>
+                <p>Original moon Tide Clock. <br></br> Different skins are available. <br></br> 10.1" display.</p>
+                <h5>$100.00</h5>
+            </div>
+            <div className="col-md-4 col-lg-4 text-center  mb-4 mb-md-0">
+                <a href="./about.html">
+                    <img src="./images/Scylla.png" alt="Charybdis Clock"></img>
+                    <h5>Tidal Scylla</h5>
+
+                </a>
+                <p>Original moon Tide Clock. <br></br> Different skins are available. <br></br> 7" display.</p>
+                <h5>$60.00</h5>
+            </div>
+
+            <div className="col-md-4 col-lg-4 text-center  mb-4 mb-md-0">
+                <a href="./about.html">
+                    <img src="./images/Charybdis_Skin.png" alt="Charybdis Clock"></img>
+                    <h5>Tidal Charysbid skins</h5>
+
+                </a>
+                <p>Original moon Tide Clock. <br></br> Different skins are available. <br></br> 10.1" display.</p>
+                <h5>$1.00-$5.00</h5>
+            </div>
+            <div className="col-md-4 col-lg-4 text-center  mb-4 mb-md-0">
+                <a href="./about.html">
+                    <img src="./images/Scylla_Skin.png" alt="Charybdis Clock"></img>
+                    <h5>Tidal Scylla skins</h5>
+
+                </a>
+                <p>Original moon Tide Clock. <br></br> Different skins are available. <br></br> 7" display.</p>
+                <h5>$1.00-$5.00</h5>
+            </div>
+        </div>
+
+    </section>
+
+
+
+
+    );
+}
+
+export default Products;
